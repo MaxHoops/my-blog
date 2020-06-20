d15ecad39e083cddc24c89ed1a36372117901f0f
MaxGitted <mobhoops@gmail.com>
2020-06-20 12:13:59 +1000

    First commit

---
+++ src/build/precache-manifest.b5c0b7ae01185dfbd3b826f4d851bd4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519dc6afc7b845795d69b15ab54878f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5256a70614a5e3922149",
+    "url": "/static/css/main.6e18a9b2.chunk.css"
+  },
+  {
+    "revision": "a1850b1e29f83404fcfa",
+    "url": "/static/js/2.11aa829e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11aa829e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5256a70614a5e3922149",
+    "url": "/static/js/main.051b4364.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
